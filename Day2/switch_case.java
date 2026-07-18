95841768f5cfb2685f143a6c8c8e19db0e709e41
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 15:28:16 +0530

    switch case statments

--- Day2/switch_case.java
+++ Day2/switch_case.java
@@ -8,6 +8,15 @@ public class switch_case {
         switch(day){
             case 1:
                 System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+            System.out.println();
         }
     }
 }

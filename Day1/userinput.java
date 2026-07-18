8c856170172732507081dbb8b940e748ce82b5f5
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 12:58:48 +0530

    taking user input

---
+++ Day1/userinput.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter Your Fav Food Item: ");
+        String food = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        String age = sc.nextLine();
+
+        System.out.println("Hello " + name + " ! You are " + age + " years old and you love " + food + ".");
+
+    }
+}

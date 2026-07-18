a51acf193fad236ea853c25c740548fb28aadc18
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 15:23:18 +0530

    moved to diff folder

---
+++ Day2/switch_case.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day(1-3): ");
+        int day = sc.nextInt();
+
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+        }
+    }
+}

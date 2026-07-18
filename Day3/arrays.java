bed1273dd49b40c931d07e79c561dea71666678d
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 16:04:38 +0530

    arrays 1d and 2d

---
+++ Day3/arrays.java
@@ -0,0 +1,40 @@
+package Day3;
+
+public class arrays {
+    public static void main(String[] args) {
+        
+        // Declaring an 1-D array:
+        int[] numbers = {10,20,30,40,50};
+        
+        // Accessing diff elements of an array:
+        System.out.println("First: "+ numbers[0]);
+        System.out.println("Last :" + numbers[numbers.length-1]);
+
+        // Printing All Elements of Array:
+        System.out.println("All Numbers:");
+        for(int i=0; i<numbers.length;i++){
+            System.out.println(numbers[i] + " ");
+        }
+        System.out.println();
+
+        // Enhanced For Loop (for-each) [READ-ONLY]
+
+        System.out.println("All Numbers:");
+        for(int num : numbers){
+            System.out.println(num + " ");
+        }
+        System.out.println();
+
+
+
+        // 2-D Array (Declaration)
+        int[][] matrix = {
+            {1,2,3},
+            {4,7,9},
+            {3,6,8}
+        };
+        
+        // Accesseing different elements
+        System.out.println("Matrix[1][1]: "+ matrix[1][1]);
+    }
+}

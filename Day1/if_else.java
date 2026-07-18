986605d33ebf6971edf1028f914bc2815d2d3fd0
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 13:26:41 +0530

    changes in code

---
+++ Day1/if_else.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class if_else {
+    public static void main(String[] args) {
+
+        int marks;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your marks: ");
+        
+
+        if (marks>=90){
+            System.out.println("Grade: A");
+        }else if (marks) {
+            
+        }
+    }
+}
--- Day1/userinput.java
+++ Day1/userinput.java
@@ -10,7 +10,7 @@ public class userinput {
         String food = sc.nextLine();
 
         System.out.print("Enter your age: ");
-        String age = sc.nextLine();
+        int age = sc.nextLine();
 
         System.out.println("Hello " + name + " ! You are " + age + " years old and you love " + food + ".");
 

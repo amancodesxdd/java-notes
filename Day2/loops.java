b5acefec5b7d1396d8129ede444ff4ffe0e08e81
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 15:39:03 +0530

    add loops

---
+++ Day2/loops.java
@@ -0,0 +1,22 @@
+public class loops {
+    public static void main(String[] args) {
+        // For Loop
+        for(int i=0;i<6;i++){
+            System.out.println(i + " ");
+        }
+        
+        // While Loop
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i + " ");
+            i++;
+        }
+
+        // Do While Loop
+        int k = 10;
+        do {
+            System.out.println(k + " ");
+            k++;
+        }while (k<=5);
+    }
+}

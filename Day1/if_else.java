539ada9fef1b51833ebe72c623bd9e4cfd58c911
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 13:32:44 +0530

    if else statements

--- Day1/if_else.java
+++ Day1/if_else.java
@@ -2,15 +2,19 @@ import java.util.*;
 public class if_else {
     public static void main(String[] args) {
 
-        int marks;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your marks: ");
+        int marks = sc.nextInt();
         
 
         if (marks>=90){
             System.out.println("Grade: A");
-        }else if (marks) {
-            
+        }else if (marks>=75) {
+            System.out.println("Grade: B");
+        }else if (marks>=50){
+            System.out.println("Grade: C");
+        }else{
+            System.out.println("FAILED!");
         }
     }
 }

8e3a57c675f608cd48d7596f3880b35ff096013f
Aman Kumar <amankr.dev17@gmail.com>
2026-07-18 12:29:58 +0530

    variables and data type

---
+++ Day1/variables_datatype.java
@@ -0,0 +1,18 @@
+public class variables_datatype {
+    public static void main(String[] args) {
+        // DATA TYPES 
+        int age = 19;
+        double gpa = 9.23;
+        char grade = 'A';
+        boolean isJavafun = true;
+        String name = "Rose";
+
+        // PRINTING ALL
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is JAVA Fun ?: " + isJavafun);
+
+    }
+}

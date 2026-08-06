28c7753185b5ed0eb2d4759f7b8f46e7fe5e83bc
Aman Kumar <amankr.dev17@gmail.com>
2026-08-06 18:32:14 +0530

    threading in java 2

--- Day6/threading.java
+++ Day6/threading.java
@@ -2,8 +2,8 @@ public class threading{
 public static void main(String[] args) {
     thread1 t1 = new thread1();
     t1.start();
-    // Thread t2 = new Thread(new thread2());
-    // t2.start(); 
+    Thread t2 = new Thread(new thread2());
+    t2.start(); 
 }
 }
 class thread1 extends Thread{
@@ -21,17 +21,17 @@ class thread1 extends Thread{
     }
 }
 
-// class thread2 implements Runnable{
-//     public void run(){
-//         for(int i=0;i <= 10; i ++){
-//             System.out.println("Rose");
+class thread2 implements Runnable{
+    public void run(){
+        for(int i=0;i <= 10; i ++){
+            System.out.println("Rose");
 
-//             try{
-//                 thread1.sleep(1000);
-//             }
-//             catch(InterruptedException e){
-//                 System.out.println("Threading Interrupted!");
-//             }
-//         }
-//     }
-// }
+            try{
+                thread1.sleep(1000);
+            }
+            catch(InterruptedException e){
+                System.out.println("Threading Interrupted!");
+            }
+        }
+    }
+}

01dd110c5fca2b11cf9b62d170fbde1b9b8610a0
Aman Kumar <amankr.dev17@gmail.com>
2026-08-06 19:02:16 +0530

    threading in java 3

--- Day6/threading.java
+++ Day6/threading.java
@@ -1,9 +1,18 @@
 public class threading{
 public static void main(String[] args) {
-    thread1 t1 = new thread1();
-    t1.start();
-    Thread t2 = new Thread(new thread2());
-    t2.start(); 
+    // thread1 t1 = new thread1();
+    // t1.start();
+    // Thread t2 = new Thread(new thread2());
+    // t2.start(); 
+    // thread3 t3 = new thread3();
+    // t3.start();
+    // thread4 t4 = new thread4();
+    // t4.start();
+    // System.out.println(Thread.currentThread().getName());
+
+    // Thread.currentThread().setName("Main Worker");
+    // System.out.println(Thread.currentThread().getName());
+
 }
 }
 class thread1 extends Thread{
@@ -21,13 +30,14 @@ class thread1 extends Thread{
     }
 }
 
+
 class thread2 implements Runnable{
     public void run(){
         for(int i=0;i <= 10; i ++){
             System.out.println("Rose");
 
             try{
-                thread1.sleep(1000);
+                Thread.sleep(1000);
             }
             catch(InterruptedException e){
                 System.out.println("Threading Interrupted!");
@@ -35,3 +45,33 @@ class thread2 implements Runnable{
         }
     }
 }
+
+class thread3 extends Thread{
+    public void run(){
+        for(int i=1;i <= 10; i +=2){
+                System.out.println("Odd: "+ i);
+            
+            try{
+                thread3.sleep(1000);
+            }
+            catch(InterruptedException e){
+                System.out.println("Threading Interrupted!");
+            }
+        }
+    }
+}
+
+class thread4 extends Thread{
+    public void run(){
+        for(int i=2;i <= 10; i +=2){
+                System.out.println("Even: "+ i);
+            try{
+                thread4.sleep(1000);
+            }
+            catch(InterruptedException e){
+                System.out.println("Threading Interrupted!");
+            }
+        }
+    }
+}
+
